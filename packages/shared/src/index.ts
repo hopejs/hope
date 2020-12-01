7261972a2a3dfac38294910f6cf0e7f04f751b64
zhangzhonghe <958414905@qq.com>
2020-12-01 13:24:20 +0800

    feat(shared): add addScopeForSelector

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -53,3 +53,27 @@ export function getScopeIdVersion(css: string, scopeId: string) {
     return match[0] + arr.join(',') + match[match.length - 1];
   });
 }
+
+export function addScopeForSelector(selector: string, scopeId: string) {
+  selector = selector.trim();
+  const arr = selector.split(',').map((str) => {
+    str = str.trim();
+    if (!str) return;
+    if (isAnimationSelector(str)) return str;
+    if (str.indexOf(':') >= 0) {
+      return str.replace(':', `[${scopeId}]:`);
+    }
+    return str + `[${scopeId}]`;
+  });
+  return arr.filter((v) => v).join(',');
+}
+
+function isAnimationSelector(selector: string) {
+  if (
+    selector === 'to' ||
+    selector === 'from' ||
+    selector[selector.length - 1] === '%'
+  )
+    return true;
+  return false;
+}
