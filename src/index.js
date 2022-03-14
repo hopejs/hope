24cf70d4a57e8c089cc8c9286cd4751542a1ce32
zhangzhonghe <958414905@qq.com>
2022-03-14 08:32:56 +0800

    chore: nothing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,44 @@
-function Button(text) {
+function button(props) {
   const dom = document.createElement('button');
-  dom.innerText = text;
+  Object.keys(props).forEach((key) => {
+    if (key === 'onClick') {
+      return dom.addEventListener('click', props[key]);
+    }
+    dom[key] = props[key];
+  });
   return dom;
 }
 
-document.querySelector('#app').appendChild(Button(1));
+/**
+ * View
+ * state <<-> view
+ */
+function Button({ number, onClick }) {
+  const [value, setValue] = useState(number)
+  const handleClick = () => {
+    const result = ++value
+    setValue(result)
+    onClick(result)
+  }
+
+  return button({ innerText: value, onClick: handleClick });
+}
+
+function State({result}) {
+  function handleClick() {
+    this.innerText = ++result
+  }
+
+  return {
+    number: result,
+    onClick: handleClick
+  }
+}
+
+document.querySelector('#app').appendChild(Button(State({result: 1})));
+
+/**
+ * 1.建立连接
+ * 2.注册事件
+ * 3.执行副作用
+ */
