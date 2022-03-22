f3997c63fee51da696f618d845161df445a54765
zhangzhonghe <958414905@qq.com>
2022-03-22 21:41:37 +0800

    draft: migrate reactivity to src

---
+++ __tests__/reactivity/reactive.test.ts
@@ -0,0 +1,31 @@
+import { reactive } from "../src";
+
+describe("reactive", () => {
+  it("object", () => {
+    expect(reactive({ name: "a" })).toEqual({ name: "a" });
+  });
+
+  it("array", () => {
+    const proxy = reactive([] as any[]);
+    expect(proxy).toEqual([]);
+
+    proxy.push(1);
+    expect(proxy).toEqual([1]);
+    proxy.push(2, 3);
+    expect(proxy).toEqual([1, 2, 3]);
+
+    proxy.pop();
+    expect(proxy).toEqual([1, 2]);
+
+    proxy.shift();
+    expect(proxy).toEqual([2]);
+
+    proxy.unshift(0, 1);
+    expect(proxy).toEqual([0, 1, 2]);
+
+    proxy.splice(0, 3, "a", "b", "c");
+    expect(proxy).toEqual(["a", "b", "c"]);
+    proxy.splice(0, 2);
+    expect(proxy).toEqual(["c"]);
+  });
+});
---
+++ src/reactivity/index.ts
@@ -0,0 +1,8 @@
+export {
+  effect,
+  stop,
+  reactive,
+  computed,
+  isReactive,
+  ReactiveEffect,
+} from '@vue/reactivity';
