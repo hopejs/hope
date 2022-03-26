4edbcd5b1ca8ba0baca8ab684d61f845a23cab37
zhangzhonghe <958414905@qq.com>
2022-03-27 00:55:17 +0800

    draft: update block

--- src/api/block.ts
+++ src/api/block.ts
@@ -14,23 +14,73 @@ import {
   resetBlockFragment,
   setBlockFragment,
 } from '@/core';
-import { LIFECYCLE_KEYS } from '@/shared';
+import { isFunction, LIFECYCLE_KEYS } from '@/shared';
 import { autoUpdate } from './autoUpdate';
 
-export function block(range: () => void) {
-  const start = createPlaceholder('block start');
-  const end = createPlaceholder('block end');
-  const container = getContainer();
-  appendChild(container, start);
-  appendChild(container, end);
+enum BlockTypes {
+  hFor = 'hFor',
+  hIf = 'hIf',
+}
 
-  const blockFragment = createBlockFragment();
-  autoUpdate(() => {
-    setBlockFragment(blockFragment);
-    range();
-    resetBlockFragment();
-    insertBlockFragment(blockFragment, start, end);
-  });
+export function block<T>(
+  type: BlockTypes.hFor,
+  value: T | (() => T),
+  range: (item: T, index: number) => void
+): void;
+export function block(
+  type: BlockTypes.hIf,
+  value: any | (() => any),
+  range: () => void,
+  elseRange: () => void
+): void;
+export function block(
+  type: BlockTypes,
+  value: any,
+  range: any,
+  elseRange?: any
+): void {
+  if (isFunction(value)) {
+    const start = createPlaceholder('block start');
+    const end = createPlaceholder('block end');
+    const container = getContainer();
+    appendChild(container, start);
+    appendChild(container, end);
+
+    const blockFragment = createBlockFragment();
+    if (type === BlockTypes.hFor) {
+      let oldValue: any[];
+      autoUpdate(() => {
+        const newValue = value();
+        if (oldValue === newValue) return;
+        oldValue = newValue;
+        setBlockFragment(blockFragment);
+        newValue.forEach((item: any, index: number) => {
+          range(item, index);
+        });
+        resetBlockFragment();
+        insertBlockFragment(blockFragment, start, end);
+      });
+    } else if (type === BlockTypes.hIf) {
+      let oldValue: any;
+      autoUpdate(() => {
+        const newValue = value();
+        if (oldValue === newValue) return;
+        oldValue = newValue;
+        setBlockFragment(blockFragment);
+        newValue ? range() : elseRange();
+        resetBlockFragment();
+        insertBlockFragment(blockFragment, start, end);
+      });
+    }
+  } else {
+    if (type === BlockTypes.hFor) {
+      value?.forEach((item: any, index: number) => {
+        range(item, index);
+      });
+    } else if (type === BlockTypes.hIf) {
+      value ? range() : elseRange();
+    }
+  }
 }
 
 function insertBlockFragment(
@@ -47,7 +97,7 @@ function remove(start: Node, end: Node, firstNode: Node | null) {
   end = firstNode || end;
   let next: any = start.nextSibling;
   // next 可能已经被 remove。
-  while(next && next !== end) {
+  while (next && next !== end) {
     // 调用元素的卸载钩子
     invokeElementUnmountedHooks(next);
     // 调用组件的卸载钩子
