899474c903f6e9d84e93e3e0bce2f2f6254e5850
zhangzhonghe <958414905@qq.com>
2022-03-28 01:15:19 +0800

    perf: remove update task when element unmounted

--- src/api/autoUpdate.ts
+++ src/api/autoUpdate.ts
@@ -1,5 +1,6 @@
 import { getLifecycleHandlers } from '@/core';
 import { getCurrentComponent } from '@/core/scheduler';
+import { onElementUnmounted } from './lifecycle';
 
 export function autoUpdate(block: () => any) {
   const { updatedHandlers } = getLifecycleHandlers();
@@ -8,10 +9,15 @@ export function autoUpdate(block: () => any) {
   if (currentComponent) {
     (currentComponent.uq || (currentComponent.uq = [])).push(block);
     currentComponent.ulh || (currentComponent.ulh = updatedHandlers);
+    onElementUnmounted(() => {
+      currentComponent.uq = currentComponent.uq?.filter(
+        (item) => item !== block
+      );
+    });
+    // onUnmounted(() => {
+    //   if (currentComponent) {
+    //     removeComponent(currentComponent);
+    //   }
+    // });
   }
-  // onUnmounted(() => {
-  //   if (currentComponent) {
-  //     removeComponent(currentComponent);
-  //   }
-  // });
 }
