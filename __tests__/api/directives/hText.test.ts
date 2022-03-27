b4e1a23ebd7f99aec63906430828374f38ce4a8d
zhangzhonghe <958414905@qq.com>
2022-03-27 21:48:09 +0800

    fix: make hText tests pass

--- __tests__/api/directives/hText.test.ts
+++ __tests__/api/directives/hText.test.ts
@@ -1,27 +1,39 @@
 import { getCurrentElement, HopeElement, nextTick } from '@/core';
 import { LIFECYCLE_KEYS } from '@/shared';
-import { div, $div, hText } from '@/api';
+import { div, $div, hText, defineComponent } from '@/api';
+import { refresh } from '@/core/scheduler';
 
 describe('hText', () => {
   it('basic', () => {
-    div();
-    hText('text');
-    expect(getCurrentElement()?.innerHTML).toBe(`text`);
-    $div();
+    const [com, $com] = defineComponent(() => {
+      div();
+      hText('text');
+      expect(getCurrentElement()?.innerHTML).toBe(`text`);
+      $div();
+    });
+
+    com();
+    $com();
   });
 
   it('reactivity', async () => {
-    const content = { value: 'text' };
+    let content = 'text';
+    let el: HopeElement;
+    const [com, $com] = defineComponent<{ text: string }>(({ props }) => {
+      div();
+      hText(() => props.text);
+      el = getCurrentElement()!;
+      $div();
+    });
 
-    div();
-    hText(() => content.value);
-    const el = getCurrentElement();
-    $div();
+    com({ text: () => content });
+    $com();
 
-    expect(el?.innerHTML).toBe(`text`);
-    content.value = '123';
+    expect(el!.innerHTML).toBe(`text`);
+    content = '123';
+    refresh();
     await nextTick();
-    expect(el?.innerHTML).toBe(`123`);
+    expect(el!.innerHTML).toBe(`123`);
   });
 
   it('elementUnmounted', () => {
