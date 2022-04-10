10f4340021eb364ba9dd9871c8bf51923112fbbb
zhangzhonghe <958414905@qq.com>
2022-04-11 01:23:22 +0800

    perf: void repeated refresh

--- src/activity/refresh.ts
+++ src/activity/refresh.ts
@@ -1,7 +1,16 @@
 import { nextTick } from '..';
 import { getCurrentScope, notify } from './makeScope';
 
+// void repeated refresh
+const memo = new WeakMap();
+
 export function refresh() {
   const currentScope = getCurrentScope();
-  currentScope && nextTick(() => notify(currentScope));
+  if (currentScope && !memo.has(currentScope)) {
+    nextTick(() => {
+      memo.delete(currentScope);
+      notify(currentScope);
+    });
+    memo.set(currentScope, true);
+  }
 }
