54d3fb4ebd249b06372e03eb6c1e4a6f6f3aec5d
zhangzhonghe <958414905@qq.com>
2022-04-16 16:54:49 +0800

    chore: export more api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,6 @@
 export * from '@/api/scheduler';
+export * from '@/activity';
 export * from '@/html';
 export { hFor } from './hFor';
 export { hIf } from './hIf';
+export { defineComponent } from '@/component';
